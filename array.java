31f74bf78e8567765c9f3635ae6cc371149f3ac5
Jyothi2005 <JyothiHudali05@gmail.com>
2025-06-09 09:33:58 +0530

    save

--- array.java
+++ array.java
@@ -1,14 +1,21 @@
 public class array{
     public static void main(String[] args) {
-        int sum=0;
+        int max=0;
+        int min=0;
         int n=5;
-        int[] scores={10,20,30,40,50};
+        int[] scores={0,20,30,40,50};
     for(int i=0;i<n;i++)
 {
-    sum+=scores[i];
+    if(min>scores[i])
+    {
+        min=scores[i];
+    }
+    if(max<scores[i])
+    {
+        max=scores[i];
+    }
 }  
-System.out.println("The sum of scores is"+sum);
-int avg=sum/5;
-System.out.println("The average of scores is"+avg);
+System.out.println("The minimum of scores is"+min);
+System.out.println("The maximum of scores is"+max);
 }
 }
